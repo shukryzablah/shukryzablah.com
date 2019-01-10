d7e1067f53d22ed6fc4a9943bb1fa5125238e788
shukryzablah <shukryzablah@gmail.com>
2019-01-09 23:10:48 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Name() {
+    return <h1 className="name">Shukry Zablah</h1>;
+}
+
+function Links() {
+    return <a href="https://www.github.com/shukryzablah">Github</a>;
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      return (
+          <div className="container">
+            <div className="info">
+              <Name />
+              <Links />
+            </div>
+          </div>
     );
   }
 }
